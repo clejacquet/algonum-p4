6451c9b51eb076c222bce9300f9719bfeadea379
THIENOT Lucile <lthienot@enseirb-matmeca.fr>
2016-03-28 16:37:46 +0200

    "NRMETHOD USING LAMBDA EXPRESSIONS - TEST 1 OK"

--- NRMethod.py
+++ NRMethod.py
@@ -5,7 +5,7 @@ import numpy as np
 
 def Newton_Raphson(f,H,U0,N,epsilon):
     U=U0
-    V=np.linalg.lstsq(H(U),-f(U))[0]
+    V=np.linalg.lstsq(H(U),-f(U))[0] #function resolving Ax=B
     UplusV=U+V
     i=1
     while(i<N and ((np.linalg.norm(UplusV-U))>epsilon)):
@@ -15,14 +15,10 @@ def Newton_Raphson(f,H,U0,N,epsilon):
         i+=1
     return UplusV
 
-def fn_ex(A):
-    return (np.array([A[0]*A[0]-2,A[1]*A[1]-3]))
-
-def H_ex(A):
-    return np.array([[(2*A[0]), 0], [0, (2*A[1])]])
-
 def main():
-    functionU=Newton_Raphson(fn_ex,H_ex,np.array([1, 1]),100,0.0001)
+    functionU=Newton_Raphson(lambda A: np.array([A[0]*A[0]-2,A[1]*A[1]-3]),lambda A:  np.array([[(2*A[0]), 0], [0, (2*A[1])]]),np.array([1, 1]),100,0.0001)
+    
+    print("TEST 1, USING f:(x,y)->(x²-2,y²-3)")
     print("Expected solution :")
     print("[~1.41421 ~1.73205]")
     print("")
