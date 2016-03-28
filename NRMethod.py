9fc1c4ca889eb4840653a4936c36e38082e80efa
THIENOT Lucile <lthienot@enseirb-matmeca.fr>
2016-03-28 16:31:11 +0200

    "NRMethod.py OK"

--- NRMethod.py
+++ NRMethod.py
@@ -5,18 +5,29 @@ import numpy as np
 
 def Newton_Raphson(f,H,U0,N,epsilon):
     U=U0
-    UplusV=U-(f(U)/H(U))
+    V=np.linalg.lstsq(H(U),-f(U))[0]
+    UplusV=U+V
     i=1
     while(i<N and ((np.linalg.norm(UplusV-U))>epsilon)):
         U=UplusV
-        UplusV=U-(f(U)/H(U))
+        V=np.linalg.lstsq(H(U),-f(U))[0]
+        UplusV=U+V
         i+=1
     return UplusV
 
-def main():
-    functionU=Newton_Raphson((lambda (x,y): (x*x-2,y*y-3)),(lambda (x,y): np.mat('[2*x 0 ; 0 2*y]')),(1,1),100,0.0001)
-    print("ok")
+def fn_ex(A):
+    return (np.array([A[0]*A[0]-2,A[1]*A[1]-3]))
 
+def H_ex(A):
+    return np.array([[(2*A[0]), 0], [0, (2*A[1])]])
+
+def main():
+    functionU=Newton_Raphson(fn_ex,H_ex,np.array([1, 1]),100,0.0001)
+    print("Expected solution :")
+    print("[~1.41421 ~1.73205]")
+    print("")
+    print("Solution we get :")
+    print(functionU)
 main()
     
     
