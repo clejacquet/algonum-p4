899ef6dfd6cff4f2c5539690e5dc804377914c4b
THIENOT Lucile <lthienot@enseirb-matmeca.fr>
2016-04-03 15:53:04 +0200

    "Tentative de courbe d'erreur"

--- NRMethod.py
+++ NRMethod.py
@@ -1,9 +1,18 @@
 # coding: utf-8
 import numpy as np
+import math as m
+import matplotlib.pyplot as plt
 import time as tm
 
 # Newton-Raphson Method
 def Newton_Raphson(f, H, U0, N, epsilon):
+    """Newton_Raphson takes 5 arguments :
+    f: studied function (given as an array)
+    H: jacobian matrix of f
+    U0: start point
+    N: maximum of iterations
+    epsilon: precision
+    and returns the calculated zeros of f """
     U=U0
     V=np.linalg.lstsq(H(U),-f(U))[0] #function resolving Ax=B
     UplusV=U+V
@@ -15,8 +24,16 @@ def Newton_Raphson(f, H, U0, N, epsilon):
         i+=1
     return UplusV
 
-
+# Newton Method with Backtracking
 def Newton_BT(f,H,U0,N,epsilon):
+    """Newton_BT takes 5 arguments :
+    f: studied function (given as an array)
+    H: jacobian matrix of f
+    U0: start point
+    N: maximum of iterations
+    epsilon: precision
+    and returns the zeros of f,
+    calculated through the Newton method with backtracking"""
     U=U0
     for i in range(1,N):
 #        print(i)
@@ -36,6 +53,21 @@ def Newton_BT(f,H,U0,N,epsilon):
         U=U+lambd*dU
     return U
 
+
+#curve representing the error on the zeros depending on the precision
+def error_NR(f,H,U0,N,real_zero,eps1,eps2,step):
+    y=[]
+    x=np.linspace(eps1,eps2,step)
+    Nzero=np.linalg.norm(real_zero)
+    while(eps1<=eps2):
+        zero=Newton_Raphson(f,H,U0,N,eps1)
+        y.append(abs(Nzero-np.linalg.norm(zero)))        
+        eps1+=step
+    plt.plot(x,y)
+    plt.ylabel("Error from the NR Method")
+    plt.xlabel("Precision of the NR Method")
+    plt.title("Error on the calculated zeros depending on the precision of the NRMethod")
+    plt.show()
 
 def main():
     functionU2D=Newton_Raphson(lambda A: np.array([A[0]*A[0]-2,A[1]*A[1]-3]),lambda A:  np.array([[(2*A[0]), 0], [0, (2*A[1])]]),np.array([1, 1]),100,0.00000001)
@@ -73,13 +105,8 @@ def main():
     #print("")
     #print("Solution we get :")
     #print(functionUBT2)
-    
-    
-## Faire tracé de l'erreur
-## Faire test 1D de NR
-## Faire Nexton Backtracking
-
-
+        
+    error_NR(lambda A: np.array([(A[0]**2)-2]),lambda A:  np.array([[2*A[0]]]),np.array([1]),100,m.sqrt(2),0.00000001,0.1,0.001)
 
 main()
 
